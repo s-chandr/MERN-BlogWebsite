f1d514730bc37412828b36983ec964772add8c39
s-chandr <chandrasayan15@gmail.com>
2023-01-14 23:44:34 +0530

    firebase integration done

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,7 @@ let db;
 
 async function connectToDb (cb) {
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ulx7nic.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
     db = client.db('react-blog-db'); //use react-blog-db JAISA
     cb();
--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,4 +1,14 @@
+
+import 'dotenv/config' ;
+
+
+//build wala step 
 // nodemon is only dev mode not for production
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 
 import express from 'express';
@@ -6,61 +16,95 @@ import express from 'express';
 //since we have mention "type": "module", so need to write .js
 import {db, connectToDb } from './db.js';
 
-const app = express();
 
+//firebase admin 
+import fs from 'fs';
+import admin from 'firebase-admin';
+const credentials = JSON.parse(
+    fs.readFileSync('./credentials.json')
+);
+admin.initializeApp({credential:  admin.credential.cert(credentials),});
+
+
+
+const app = express();
 app.use(express.json());
 // adding a middle ware ;
+
+
+// build folder
+app.use(express.static(path.join(__dirname,'../build')));
+// route handler anything other than our api's (handles all that don't start with /api)
+app.get(/^(?!\/api).+/ , (req,res)=>{
+    res.sendFile(path.join(__dirname,'../build/index.html'));
+});
+
+
+///adding middleware to use auth token includesd in headers
+app.use(async(req,res,next)=>{
+    // bhai name waam sahi se likha karo bc authtoken me token ka t Capital likhdiya tha toh chal hi nhi rha tha 
+    const {authtoken } = req.headers ;
+    
+    //null a skte hai even if the usr is not logged in kyoki axios null bhejdega aur if bhi chal jayega 
+    if(authtoken )
+    {   try{
+            req.user = await admin.auth().verifyIdToken(authtoken );
+        }
+        catch (e) {
+           return  res.sendStatus(400);
+        }
+    }
+    req.user = req.user || {};
+    next();
+})
 
 //async
 app.get('/api/articles/:name', async (req, res) => {
     const {name} = req.params;
+    
+    const {uid} = req.user;
+
     const article = await db.collection('articles').findOne({ name }); 
-    if(article)
-    res.json(article);
+    if(article){
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.includes(uid); 
+        res.json(article);    
+    }
     else
     res.status(404).send("Article Not Found");
 });
 
-// //sample array db
-// let articleInfo =[{
-//     name:"learn-react",
-//     upvotes:0,
-//     comments:[],
-// },{
-//     name:"learn-node",
-//     upvotes:0,
-//     comments:[]
-// },{
-//     name:"mongoDb",
-//     upvotes:0,
-//     comments:[],
-// }]
 
 
 
-
-
-
-
-
-app.put('/api/articles/:name/upvote',async(request,response) =>{
-
-    const {name} = request.params;
+app.use((req,res,next)=>{
+    if(req.user){
+        next();
+    }
+    else    
+    res.sendStatus(401);
+})
+app.put('/api/articles/:name/upvote',async(req,res) =>{
+    const {uid} = req.user;
     
-    
-    
-    await db.collection('articles').updateOne({ name } , {
-        $inc:{upvotes:1},
-    }); 
-
-    const article = await db.collection('articles').findOne({ name });
-    if(article)
-    {   
-        // article.upvotes += 1;
-        response.send(article);
+    const {name} = req.params;
+    const article = await db.collection('articles').findOne({ name }); 
+    // console.log(uid);
+    if(article){
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.includes(uid); 
+        
+        if(canUpvote){
+        await db.collection('articles').updateOne({ name } , {
+            $inc:{upvotes:1},
+            $push:{upvoteIds :uid},
+        }); 
+    }
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle );
     }
     else
-        response.send( `The ${name} has not exist`);
+        res.send( `The ${name} has not exist`);
 
 
 });
@@ -71,22 +115,22 @@ app.put('/api/articles/:name/upvote',async(request,response) =>{
 
 
 
-app.post('/api/articles/:name/comments',async(request,response) =>{
-    const {name} = request.params;
-    const {postedBy , text } = request.body;
-    
+app.post('/api/articles/:name/comments',async(req,res) =>{
+    const {name} = req.params;
+    const { text } = req.body;
+    const {email} = req.user;
 
     
     await db.collection('articles').updateOne({ name } , {
-        $push:{comments:{postedBy , text }},
+        $push:{comments:{postedBy: email  , text }},
     });
     const article = await db.collection('articles').findOne({ name });
     if(article){
         
-        response.json( article );
+        res.json( article );
     }
     else
-        response.send( `The ${name} has not exist`);
+        res.send( `The ${name} has not exist`);
 
 });
 
@@ -94,11 +138,11 @@ app.post('/api/articles/:name/comments',async(request,response) =>{
 
 
 
-
+const PORT = process.env.port || 8000 ;
 
 
 connectToDb( ()=> {
-    app.listen(8000, () =>{
-        console.log("Server is Listening on port 8000");
+    app.listen(PORT, () =>{
+        console.log("Server is Listening on port "+PORT);
     });
 })
